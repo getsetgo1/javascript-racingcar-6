c4d6c7c3e959451f0b416cf975cebb5d57bd2fb7
getsetgo1 <dnwls1313@gmail.com>
2023-10-30 16:31:36 +0900

    입력 및 입력 이름 판단

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
 class App {
-  async play() {}
+  async play() {
+    const cars = await this.getCars();
+    const rounds = await this.getRounds();
+  }
+  async getCars() {
+    const carsInput = await MissionUtils.Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+    this.carsCheck(carsInput);
+    return carsInput;
+  }
+  carsCheck(carsInput) {
+    const cars = carsInput.split(",").forEach((car) => {
+      if (car.length > 5) throw Error("[ERROR] 숫자가 잘못된 형식입니다.");
+    });
+  }
+  async getRounds() {
+    const roundsInput = await MissionUtils.Console.readLineAsync(
+      "시도할 횟수는 몇 회인가요?"
+    );
+    return roundsInput;
+  }
 }
 
 export default App;
