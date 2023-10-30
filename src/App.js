e2e363c405c5de5611735d4e46f5b12b97d22239
getsetgo1 <dnwls1313@gmail.com>
2023-10-30 18:26:39 +0900

    랜덤 넘버 전진 함수 구현중

--- src/App.js
+++ src/App.js
@@ -3,28 +3,42 @@ class App {
   async play() {
     do {
       const cars = await this.getCars();
-      const rounds = await this.getRounds();
-    } while (rounds > 0);
+      let rounds = await this.getRounds();
+      const roundsCars = this.randomNumberForwards(cars);
+      rounds--;
+    } while (this.rounds > 0);
   }
   async getCars() {
     const carsInput = await MissionUtils.Console.readLineAsync(
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
     );
-    this.carsCheck(carsInput);
-    return carsInput;
+    const carsList = this.carsCheck(carsInput);
+    return carsList;
   }
+
   carsCheck(carsInput) {
     const cars = carsInput.split(",").map((car) => {
       if (car.length > 5) throw Error("[ERROR] 숫자가 잘못된 형식입니다.");
       return { name: car, score: 0 };
     });
-    console.log(cars);
+    return cars;
   }
   async getRounds() {
     const roundsInput = await MissionUtils.Console.readLineAsync(
-      "시도할 횟수는 몇 회인가요?"
+      "시도할 횟수는 몇 회인가요?\n"
     );
     return roundsInput;
+  }
+  randomNumberForwards(cars) {
+    const forwardCars = this.cars.map((car) => {
+      const random = MissionUtils.Random.pickNumberInRange(0, 9);
+      if (random >= 4) {
+        car.score = car.score + random - 4;
+      }
+      console.log(car);
+      return car;
+    });
+    return forwardCars;
   }
 }
 
